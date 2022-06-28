1e6849fe119fbc9da09f7f5f0dfe27c5a1307975
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-28 19:46:46 +0200

    Add axios package and testing api request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Landing from 'pages/Landing';
+import RegisterLogin from 'pages/RegisterLogin';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
         <Route path='/home' element={<h1>Homepage</h1>} />
         <Route path='/landing' element={<Landing />} />
         <Route path='/register' element={<h1>Signup page</h1>} />
-        <Route path='/login' element={<h1>Login page</h1>} />
+        <Route path='/login' element={<RegisterLogin />} />
         <Route path='*' element={<h1>Error page</h1>} />
       </Routes>
     </BrowserRouter>
--- client/src/pages/RegisterLogin/index.jsx
+++ client/src/pages/RegisterLogin/index.jsx
@@ -1,9 +1,26 @@
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 import styles from './style.module.css';
 import Logo from 'components/Logo';
 
 const RegisterLogin = () => {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .post(
+        'http://localhost:3100/users/login',
+        {
+          email: 'test@test.fr',
+          password: 'Test-123456'
+        },
+        { withCredentials: true }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div>
