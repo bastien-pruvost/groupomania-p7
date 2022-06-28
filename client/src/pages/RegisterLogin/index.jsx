130e37c087e181b5adb2a5fda22f1bd1c2fbbf31
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-28 15:58:21 +0200

    Add useEffect on RegisterLogin page

--- client/src/pages/RegisterLogin/index.jsx
+++ client/src/pages/RegisterLogin/index.jsx
@@ -3,6 +3,8 @@ import styles from './style.module.css';
 import Logo from 'components/Logo';
 
 const RegisterLogin = () => {
+  useEffect(() => {}, []);
+
   return (
     <div>
       <h1>Register Login</h1>
