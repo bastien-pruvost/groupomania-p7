2322d64253e99e666e1945cb8e7e1a3cdecc8c66
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-29 21:01:19 +0200

    Add error handler for axios requests

---
+++ client/src/utils/errors.utils.js
@@ -0,0 +1,19 @@
+export const handleError = (error) => {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.status, error.response.data);
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message);
+  }
+  // console.log(error.config);
+  if (error.response.data) {
+    return error.response.data;
+  }
+};
