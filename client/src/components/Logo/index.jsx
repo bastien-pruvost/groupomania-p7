96446231c3a6541a4cfabc9c87d48e38b23aa1ff
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-29 11:14:59 +0200

    Rename Logo component

--- client/src/components/Logo/index.jsx
+++ client/src/components/Logo/index.jsx
@@ -1,7 +1,7 @@
-import styles from './style.module.css';
+import styles from './Logo.module.css';
 import logo from 'assets/images/logo-color.svg';
 
-const index = () => {
+const Logo = () => {
   return <img src={logo} alt='Logo Groupomania' className={styles.Logo} />;
 };
-export default index;
+export default Logo;
