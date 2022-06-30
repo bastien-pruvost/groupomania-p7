fc59a8e408de30a28e89724b1e6da1a91423d515
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-30 22:46:09 +0200

    Add auth services file for api requests

---
+++ client/src/services/auth.services.js
@@ -0,0 +1,27 @@
+import { api } from 'utils/axios.utils';
+import { handleError } from 'utils/errors.utils';
+
+export const signinRequest = async (data) => {
+  try {
+    const response = await api.post('/users/signin', data);
+    return response.data;
+  } catch (err) {
+    const errorData = handleError(err);
+    throw errorData;
+  }
+};
+
+export const signupRequest = async (data) => {
+  try {
+    const response = await api.post('/users/signup', data);
+    return response.data;
+  } catch (err) {
+    const errorData = handleError(err);
+    throw errorData;
+  }
+};
+
+export const getCurrentUserRequest = async () => {
+  const response = await api.get('/users/verifyauth');
+  return response.data;
+};
