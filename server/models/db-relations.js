0f3e1500a9f3d42856cce32bc74c6d54194cd273
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-01 22:15:21 +0200

    Add all tables and relations in sequelize

--- server/models/db-relations.js
+++ server/models/db-relations.js
@@ -1,5 +1,6 @@
 const User = require('./user.model');
 const Post = require('./post.model');
+const Comment = require('./comment.model');
 
 // Post -> userId (Post author)
 User.hasMany(Post, {
@@ -7,9 +8,28 @@ User.hasMany(Post, {
   onDelete: 'CASCADE',
   onUpdate: 'CASCADE'
 });
-
 Post.belongsTo(User);
 
-// User <- UserLikePost -> Post (Post likes)
+// Comment -> userId (Comment author)
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
+Comment.belongsTo(User);
+
+// Comment -> postId (Comment on Post)
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
+Comment.belongsTo(Post);
+
+// User <- UserLikePost -> Post (Post liked by User)
 User.belongsToMany(Post, { through: 'user_like_post' });
 Post.belongsToMany(User, { through: 'user_like_post' });
+
+// User <- UserLikeComment -> Post (Comment liked by User)
+User.belongsToMany(Comment, { through: 'user_like_comment' });
+Comment.belongsToMany(User, { through: 'user_like_comment' });
