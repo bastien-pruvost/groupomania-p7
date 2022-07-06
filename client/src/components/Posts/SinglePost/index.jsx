e53ca7af81fe80e90f11511d9691ea34c213002a
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-06 13:47:03 +0200

    Add SinglePost component

--- client/src/components/Posts/SinglePost/index.jsx
+++ client/src/components/Posts/SinglePost/index.jsx
@@ -1,10 +1,32 @@
+import PostContainer from 'components/Posts/PostContainer';
+import defaultProfilePic from 'assets/images/default-profile-pic.jpg';
+import randomPic from 'assets/images/random-pic.jpg';
 import styles from './SinglePost.module.css';
+import Icon from 'components/Icon';
 
-const SinglePost = () => {
+const SinglePost = ({ post }) => {
+  // const { id, content, imagePath, createdAt, likeCount, user } = post;
   return (
-    <div className={styles.SinglePost}>
-      <h1>SinglePost</h1>
-    </div>
+    <PostContainer>
+      <article className={styles.SinglePost}>
+        <div className={styles.top_row}>
+          <img
+            className={styles.user_pic}
+            src={defaultProfilePic}
+            alt='Photo de profil'
+          />
+          <div className={styles.name_time_container}>
+            <span className={styles.name_text}>jacouille la fripouille</span>
+            <span className={styles.time_text}>Il y a 15h</span>
+          </div>
+          <Icon size='32' />
+        </div>
+        <p className={styles.content_text}>
+          Magnifique la Valle Del Cocora en Colombie !
+        </p>
+        <img className={styles.image} src={randomPic} alt='random' />
+      </article>
+    </PostContainer>
   );
 };
 
