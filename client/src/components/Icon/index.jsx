96b3b24a1651f79aa10cabb923e4c5d540da9225
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-06 13:46:46 +0200

    Add Icon component

---
+++ client/src/components/Icon/index.jsx
@@ -0,0 +1,22 @@
+import styles from './Icon.module.css';
+
+const Icon = ({ icon, size, color }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      strokeWidth={2}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      stroke={color ? color : 'currentColor'}
+      width={size}
+      height={size}
+    >
+      <circle cx={12} cy={12} r={1} />
+      <circle cx={19} cy={12} r={1} />
+      <circle cx={5} cy={12} r={1} />
+    </svg>
+  );
+};
+
+export default Icon;
