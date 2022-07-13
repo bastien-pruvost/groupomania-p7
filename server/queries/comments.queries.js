b313c3ffbd3c29b4bbaee90a9d6fff9a1b472c75
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-13 16:48:13 +0200

    Add query to create a comment

---
+++ server/queries/comments.queries.js
@@ -0,0 +1,18 @@
+const { Op } = require('sequelize');
+const Post = require('../models/post.model');
+const User = require('../models/user.model');
+const Comment = require('../models/comment.model');
+const UserLikePost = require('../models/user_like_post.model');
+const UserLikeComment = require('../models/user_like_comment.model');
+
+exports.findCommentById = (commentId) =>
+  Comment.findByPk(commentId, {
+    attributes: ['id', 'userId', 'postId', 'content']
+  });
+
+exports.saveNewComment = (newComment) => Comment.create(newComment);
+
+exports.updateCommentById = (updatedComment, commentId) =>
+  Post.update(updatedComment, { where: { id: commentId } });
+
+exports.deleteCommentById = (commentId) => Post.destroy({ where: { id: commentId } });
