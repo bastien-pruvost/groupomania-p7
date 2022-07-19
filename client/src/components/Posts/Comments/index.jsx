6b32ec35e8f5095913ec707f2ffa2a2544f31ce4
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-19 11:43:42 +0200

    Add text length limit and count limit on comments

--- client/src/components/Posts/Comments/index.jsx
+++ client/src/components/Posts/Comments/index.jsx
@@ -1,13 +1,35 @@
 import CommentForm from 'components/Posts/Comments/CommentForm';
 import SingleComment from 'components/Posts/Comments/SingleComment';
+import { useEffect, useState } from 'react';
 import styles from './Comments.module.css';
 
 const Comments = ({ comments, postId, setPostData }) => {
+  const [showAllComments, setShowAllComments] = useState(false);
+  useEffect(() => {
+    if (comments.length <= 1) {
+      setShowAllComments(true);
+    }
+  }, []);
+
   return (
     <div className={styles.Comments}>
-      {comments.map((comment) => (
-        <SingleComment key={comment.id} comment={comment} setPostData={setPostData} />
-      ))}
+      {showAllComments ? (
+        comments.map((comment) => (
+          <SingleComment key={comment.id} comment={comment} setPostData={setPostData} />
+        ))
+      ) : (
+        <SingleComment comment={comments[0]} setPostData={setPostData} />
+      )}
+
+      {!showAllComments && (
+        <button
+          className={`${styles.show_all_btn} limit-text-btn`}
+          onClick={() => setShowAllComments(true)}
+        >
+          Voir tous les commentaires
+        </button>
+      )}
+
       <CommentForm postId={postId} setPostData={setPostData} />
     </div>
   );
