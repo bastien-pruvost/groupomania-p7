9b6ae2a3989ce4670ebf48faf61f8a6dab657a03
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-21 11:44:15 +0200

    Change response for getUserProfile request

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -4,7 +4,7 @@ exports.getUserProfile = async (req, res) => {
   try {
     const userId = req.params.userId;
     const userProfile = await findUserProfileById(userId);
-    res.status(200).json({ message: userProfile });
+    res.status(200).json({ userProfile });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
