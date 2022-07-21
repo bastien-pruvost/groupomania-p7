76783dafb44a39d488def2f31bca5d4d91990fec
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-21 09:58:03 +0200

    Add Profile Page

--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,10 +1,32 @@
 import styles from './ProfilePage.module.css';
+import randomPic from 'assets/images/random-pic.jpg';
+import defaultProfilePic from 'assets/images/default-profile-pic.jpg';
 
 const ProfilePage = () => {
   return (
-    <div className={styles.ProfilePage}>
-      <h1>ProfilePage</h1>
-    </div>
+    <>
+      <div className={styles.user_profile_container}>
+        <img className={styles.cover_pic} src={randomPic} alt='Photo de couverture' />
+        <div className={styles.description_grid}>
+          <div className={styles.infos_column}>
+            <img className={styles.profile_pic} src={defaultProfilePic} alt='' />
+            <h2 className={styles.name}>Bastien Pruvost</h2>
+            <span className={styles.profession}>Developpeur Web</span>
+
+            <span className={styles.info_item}>City</span>
+            <span className={styles.info_item}>Phone</span>
+            <span className={styles.info_item}>LinkedIn</span>
+            <span className={styles.info_item}>Birth Date</span>
+          </div>
+
+          <p className={styles.bio_column}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam corrupti dolorum qui
+            rerum possimus. Culpa iste at totam quae praesentium fugit accusamus deserunt, dolor
+            architecto delectus tempore odit asperiores in ab modi!
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
