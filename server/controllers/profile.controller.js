4aae631c99b4996015d89f1f24041eedb7908f0a
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-24 14:22:46 +0200

    Fix eslint error (destructuring)

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -2,7 +2,7 @@ const { findUserProfileById } = require('../queries/users.queries');
 
 exports.getUserProfile = async (req, res) => {
   try {
-    const userId = req.params.userId;
+    const { userId } = req.params;
     const userProfile = await findUserProfileById(userId);
     res.status(200).json({ userProfile });
   } catch (err) {
