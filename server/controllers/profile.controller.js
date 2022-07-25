60966592367359bc0393547f81a2dc9e52ce1daf
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-25 08:53:07 +0200

    Update controller to edit user profile

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -13,10 +13,19 @@ exports.getUserProfile = async (req, res) => {
 exports.updateUserInfos = async (req, res) => {
   try {
     const { userId } = req.params;
-    const updatedInfos = req.body;
-    console.log(updatedInfos);
-    await updateUserInfosById(userId, updatedInfos);
-    res.status(200).json({});
+    const { body } = req;
+    const updatedData = {};
+    if (body.lastname) updatedData.lastname = body.lastname;
+    if (body.firstname) updatedData.firstname = body.firstname;
+    if (body.profession) updatedData.profession = body.profession;
+    if (body.birthDate) updatedData.birthDate = body.birthDate;
+    if (body.phoneNumber) updatedData.phoneNumber = body.phoneNumber;
+    if (body.linkedinUrl) updatedData.linkedinUrl = body.linkedinUrl;
+    if (body.city) updatedData.city = body.city;
+    if (body.bio) updatedData.bio = body.bio;
+    await updateUserInfosById(userId, updatedData);
+    const updatedProfile = await findUserProfileById(userId);
+    res.status(200).json({ profile: updatedProfile });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
