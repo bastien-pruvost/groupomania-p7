d7349ae63eda7a7a20b1809aea013a36538c250f
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-26 08:17:57 +0200

    Add UserDescription component for Profile Page

---
+++ client/src/pages/ProfilePage/UserDescription/index.jsx
@@ -0,0 +1,69 @@
+import styles from './UserDescription.module.css';
+import randomPic from 'assets/images/random-pic.jpg';
+import defaultProfilePic from 'assets/images/default-profile-pic.jpg';
+import IconMapPin from 'components/Icons/IconMapPin';
+import IconPhone from 'components/Icons/IconPhone';
+import IconLinkedin from 'components/Icons/IconLinkedin';
+import IconCalendar from 'components/Icons/IconCalendar';
+import useProfile from 'hooks/useProfile';
+
+const UserDescription = () => {
+  const { userProfile } = useProfile();
+  const {
+    firstname,
+    lastname,
+    profilePicPath,
+    coverPicPath,
+    profession,
+    birthDate,
+    city,
+    phoneNumber,
+    linkedinUrl,
+    bio
+  } = userProfile;
+
+  return (
+    <div className={styles.UserDescription}>
+      <img className={styles.cover_pic} src={randomPic} alt='Photo de couverture' />
+      <div className={styles.description_flex}>
+        <div className={styles.infos_column}>
+          <img className={styles.profile_pic} src={defaultProfilePic} alt='' />
+          <h2 className={styles.name}>{`${firstname} ${lastname}`}</h2>
+          {profession && <span className={styles.profession}>{profession}</span>}
+        </div>
+
+        <div className={styles.info_container}>
+          {city && (
+            <div className={styles.info_item}>
+              <IconMapPin size={20} /> <span>{city}</span>
+            </div>
+          )}
+          {birthDate && (
+            <div className={styles.info_item}>
+              <IconCalendar size={20} />
+              <span>{birthDate}</span>
+            </div>
+          )}
+          {phoneNumber && (
+            <div className={styles.info_item}>
+              <IconPhone size={20} />
+              <span>{phoneNumber}</span>
+            </div>
+          )}
+          {linkedinUrl && (
+            <div className={styles.info_item}>
+              <IconLinkedin size={20} />
+              <a href={linkedinUrl} target='_blank'>
+                LinkedIn
+              </a>
+            </div>
+          )}
+        </div>
+
+        {bio && <p className={styles.bio_column}>{bio}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default UserDescription;
--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,69 +1,9 @@
-import styles from './ProfilePage.module.css';
-import randomPic from 'assets/images/random-pic.jpg';
-import defaultProfilePic from 'assets/images/default-profile-pic.jpg';
-import IconMapPin from 'components/Icons/IconMapPin';
-import IconPhone from 'components/Icons/IconPhone';
-import IconLinkedin from 'components/Icons/IconLinkedin';
-import IconCalendar from 'components/Icons/IconCalendar';
-import useProfile from 'hooks/useProfile';
+import UserDescription from 'pages/ProfilePage/UserDescription';
 
 const ProfilePage = () => {
-  const { userProfile } = useProfile();
-  const {
-    firstname,
-    lastname,
-    profilePicPath,
-    coverPicPath,
-    profession,
-    birthDate,
-    city,
-    phoneNumber,
-    linkedinUrl,
-    bio
-  } = userProfile;
-
   return (
     <>
-      <div className={styles.user_profile_container}>
-        <img className={styles.cover_pic} src={randomPic} alt='Photo de couverture' />
-        <div className={styles.description_flex}>
-          <div className={styles.infos_column}>
-            <img className={styles.profile_pic} src={defaultProfilePic} alt='' />
-            <h2 className={styles.name}>{`${firstname} ${lastname}`}</h2>
-            {profession && <span className={styles.profession}>{profession}</span>}
-          </div>
-
-          <div className={styles.info_container}>
-            {city && (
-              <div className={styles.info_item}>
-                <IconMapPin size={20} /> <span>{city}</span>
-              </div>
-            )}
-            {birthDate && (
-              <div className={styles.info_item}>
-                <IconCalendar size={20} />
-                <span>{birthDate}</span>
-              </div>
-            )}
-            {phoneNumber && (
-              <div className={styles.info_item}>
-                <IconPhone size={20} />
-                <span>{phoneNumber}</span>
-              </div>
-            )}
-            {linkedinUrl && (
-              <div className={styles.info_item}>
-                <IconLinkedin size={20} />
-                <a href={linkedinUrl} target='_blank'>
-                  LinkedIn
-                </a>
-              </div>
-            )}
-          </div>
-
-          {bio && <p className={styles.bio_column}>{bio}</p>}
-        </div>
-      </div>
+      <UserDescription />
     </>
   );
 };
