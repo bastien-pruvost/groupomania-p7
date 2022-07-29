ff0f76998572423242982d8bc4907c69586c13e7
Bastien Pruvost <bastien.mory@gmail.com>
2022-07-29 12:03:14 +0200

    Add info icon component

---
+++ client/src/components/Icons/IconInfo.jsx
@@ -0,0 +1,23 @@
+// import styles from './Icon.module.css';
+
+const IconInfo = ({ size, color }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      strokeWidth={2}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      stroke={color ? color : 'currentColor'}
+      fill='none'
+      width={size}
+      height={size}
+    >
+      <circle cx={12} cy={12} r={10} />
+      <line x1={12} y1={16} x2={12} y2={12} />
+      <line x1={12} y1={8} x2='12.01' y2={8} />
+    </svg>
+  );
+};
+
+export default IconInfo;
