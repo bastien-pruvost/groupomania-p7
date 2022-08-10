171e022ebfc09aae1a223ee41103253453313fbc
Bastien Pruvost <bastien.mory@gmail.com>
2022-08-10 22:30:24 +0200

    Fix layout bug when refreshing posts list

--- client/src/hooks/useInfiniteScroll.js
+++ client/src/hooks/useInfiniteScroll.js
@@ -5,15 +5,13 @@ const useInfiniteScroll = (userId) => {
   const [postsData, setPostsData] = useState([]);
   const [lastPostId, setLastPostId] = useState(null);
   const [allPostsLoaded, setAllPostsLoaded] = useState(false);
-  const [page, setPage] = useState(1);
   const isComponentMounted = useRef(false);
+  const populatePostsRef = useRef(null);
   const scrollRef = useRef(null);
-  const pageRef = useRef(null);
-  pageRef.current = page;
-
   const limitPerPage = 8;
 
-  const getPaginatePosts = async () => {
+  const populatePosts = async (args) => {
+    const refreshList = args?.refreshList;
     try {
       const posts = userId
         ? await getUserPaginatePostsQuery(lastPostId, limitPerPage, userId)
@@ -23,38 +21,35 @@ const useInfiniteScroll = (userId) => {
       } else {
         setLastPostId(posts[posts.length - 1].id);
       }
-      const newPosts = [...postsData, ...posts];
+      const newPosts = refreshList ? [...posts] : [...postsData, ...posts];
       setPostsData(newPosts);
     } catch (err) {
       console.log(err);
     }
   };
 
-  const refreshPostsData = () => {
+  populatePostsRef.current = populatePosts;
+
+  const refreshPostsData = useCallback(() => {
     setAllPostsLoaded(false);
-    setPostsData([]);
     setLastPostId(null);
-    setPage((prevPage) => {
-      if (prevPage === 1) return 0;
-      return 1;
-    });
-  };
+    populatePosts({ refreshList: true });
+  }, []);
 
   const handleInfiniteScroll = useCallback((entries) => {
     const target = entries[0];
     if (target.isIntersecting) {
-      setPage(pageRef.current + 1);
+      populatePostsRef.current();
     }
   }, []);
 
   useEffect(() => {
-    const currentScrollRef = scrollRef.current;
     const infiniteScrollObserver = new IntersectionObserver(handleInfiniteScroll, {
       rootMargin: '500px'
     });
 
     setTimeout(() => {
-      if (currentScrollRef) infiniteScrollObserver.observe(currentScrollRef);
+      if (scrollRef.current) infiniteScrollObserver.observe(scrollRef.current);
     }, 1000);
 
     return () => {
@@ -71,8 +66,8 @@ const useInfiniteScroll = (userId) => {
   }, [userId]);
 
   useEffect(() => {
-    if (!allPostsLoaded) getPaginatePosts();
-  }, [page]);
+    populatePosts();
+  }, []);
 
   return { postsData, refreshPostsData, allPostsLoaded, scrollRef };
 };
